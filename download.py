254860e17d018080d297685e8fb03b35b3ee8883
Max Hager <maxhager28@gmail.com>
2023-08-03 18:56:14 +0200

    n

--- download.py
+++ download.py
@@ -1,25 +1,21 @@
-# In this file, we define download_model
-# It runs during container build time to get model weights built into the container
-# In this example: A Huggingface BERT model
-from transformers import pipeline
-from transformers import AutoTokenizer, pipeline, logging
-from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+from transformers import AutoTokenizer
+from auto_gptq import AutoGPTQForCausalLM
 
-def download_model():
-    model_name_or_path = "TheBloke/vicuna-7B-v1.3-GPTQ"
-    model_basename = "vicuna-7b-v1.3-GPTQ-4bit-128g.no-act.order"
-    use_triton = False
-    print("downloading model...")
-    AutoGPTQForCausalLM.from_quantized(model_name_or_path,
-        model_basename=model_basename,  
+MODEL_NAME_OR_PATH = "TheBloke/vicuna-7B-v1.3-GPTQ"
+MODEL_BASENAME = "vicuna-7b-v1.3-GPTQ-4bit-128g.no-act.order"
+DEVICE = "cuda:0"
+
+def download_model() -> tuple:
+    """Download the model and tokenizer."""
+    model = AutoGPTQForCausalLM.from_quantized(MODEL_NAME_OR_PATH,
+        model_basename=MODEL_BASENAME,  
         use_safetensors=True,
         trust_remote_code=True,
-        device="cuda:0",
-        use_triton=use_triton,
+        device=DEVICE,
+        use_triton=False,
         quantize_config=None)   
-   
-    print("downloading tokenizer...")
-    AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_OR_PATH, use_fast=True)
+    return model, tokenizer
 
 if __name__ == "__main__":
     download_model()
